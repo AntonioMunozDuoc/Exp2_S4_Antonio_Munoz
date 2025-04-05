ed9d3abe0a711ec36c65b9f6bea313c7f01958d3
AntonioMunozDuoc <ant.munozn@duocuc.cl>
2025-04-05 15:15:42 -0300

    Avances

--- src/main/java/com/mycompany/exp2_s4_antonio_munoz/Exp2_S4_Antonio_Munoz.java
+++ src/main/java/com/mycompany/exp2_s4_antonio_munoz/Exp2_S4_Antonio_Munoz.java
@@ -8,11 +8,12 @@ public class Exp2_S4_Antonio_Munoz {
         //Declaramos la clase scanner
         Scanner scanner = new Scanner(System.in);
         
+        //Variables a usar en el algoritmo
         int descuento = 0, edad, numeroAsiento, fila, seguir;
         double precioFilaA = 15000, precioFilaB = 12000, precioFilaC = 10000;
-        boolean edadValida = false, asientoComprado;
+        boolean edadValida = false;
         
-        
+        //Lista con las filas de asientos
         String [] columnas  = {"   ", "|1|", "|2|", "|3|", "|4|", "|5|"};
         String [] filaA = {" A ", "|_|", "|_|", "|_|", "|_|", "|_|"};
         String [] filaB = {" B ", "|_|", "|_|", "|_|", "|_|", "|_|"};
@@ -22,10 +23,12 @@ public class Exp2_S4_Antonio_Munoz {
         System.out.println("Bienvenido al sistema de ventas de entradas del Teatro Moro");
         
         //Ingreso de edad del usuario para obtener descuento
+        //Para efectos de la tarea se considerara a los mayores de 60 como adultos mayores
+        //Y se considerara a los jovenes de 18 hacia abajo como estudiantes
         while (!edadValida){
             System.out.println("Por favor ingrese su edad:");
             edad = scanner.nextInt();
-            if (edad >= 60 && edad <= 110) {
+            if (edad >= 60 && edad <= 110) { 
                 edadValida = true;
                 descuento  = 15;
             }else if (edad <= 18 && edad >= 0) {
@@ -46,14 +49,13 @@ public class Exp2_S4_Antonio_Munoz {
             System.out.println("2 - Salir");
             System.out.println("Ingrese el número de la opcion que desea elegir");
             opcionMenu = scanner.nextInt();
-            asientoComprado = false;
             System.out.println("=========================================================");
             
             switch (opcionMenu){       
                 case 1:
-                    while (!asientoComprado){
-                        System.out.println("A continuacion le mostraremos una grafica con los asientos del teatro");
+                    System.out.println("A continuacion le mostraremos una grafica con los asientos del teatro");
                     System.out.println("Donde |_| corresponde a los asientos vacios y |X| corresponde a los asientos en uso");
+                    //Muestra grafica de asientos disponibles
                     for (int i = 0; i < 6; i++){
                         System.out.print(columnas[i]);
                     }
@@ -71,11 +73,13 @@ public class Exp2_S4_Antonio_Munoz {
                     }
                     System.out.println("");
                     
+                        //Seleccion de la fila
                         System.out.println("Por favor seleccione la fila que desea comprar");
                         System.out.println("1- A   2- B   3- C");
                         fila = scanner.nextInt();
                         
                         if (fila < 4 && fila != 0) {
+                            //Seleccion del numero de asiento
                              System.out.println("Ahora seleccione el número del asiento que desea comprar");
                             numeroAsiento = scanner.nextInt();
                             
@@ -86,6 +90,7 @@ public class Exp2_S4_Antonio_Munoz {
                                         if (filaA[numeroAsiento] == "|X|"){
                                             System.out.println("El asiento seleccionado ya esta ocupado");
                                         }else{
+                                            //Muestra de todos los datos relacionados a la compra fila A
                                              System.out.println("Usted eligio el asiento " + numeroAsiento + " de la fila A");
                                              System.out.println("El precio base de la entrada es $" + precioFilaA);
                                              System.out.println("Usted tiene un descuento de %" + descuento);
@@ -98,6 +103,7 @@ public class Exp2_S4_Antonio_Munoz {
                                         if (filaB[numeroAsiento] == "|X|"){
                                             System.out.println("El asiento seleccionado ya esta ocupado");
                                         }else{
+                                            //Muestra de todos los datos relacionados a la compra fila B
                                             System.out.println("Usted eligio el asiento " + numeroAsiento + " de la fila B");
                                             System.out.println("El precio base de la entrada es $" + precioFilaB);
                                             System.out.println("Usted tiene un descuento de %" + descuento);
@@ -109,6 +115,7 @@ public class Exp2_S4_Antonio_Munoz {
                                         if (filaC[numeroAsiento] == "|X|"){
                                             System.out.println("El asiento seleccionado ya esta ocupado");
                                         }else{
+                                            //Muestra de todos los datos relacionados a la compra fila C
                                             System.out.println("Usted eligio el asiento " + numeroAsiento + " de la fila C");
                                             System.out.println("El precio base de la entrada es $" + precioFilaC);
                                             System.out.println("Usted tiene un descuento de %" + descuento);
@@ -117,6 +124,7 @@ public class Exp2_S4_Antonio_Munoz {
                                         }
                                         break;
                                 }
+                                //Pregunta si se desea continuar realizando compras o se desea salir del programa
                                 System.out.println("¿Desea continuar comprando?");
                                 System.out.println("1- Si   2-No");
                                 seguir = scanner.nextInt();
@@ -125,7 +133,6 @@ public class Exp2_S4_Antonio_Munoz {
                                 }else if (seguir != 1){
                                     System.out.println("Opción invalida");
                                 }
-                                asientoComprado = true;
                             }else{
                                 System.out.println("Opción no valida, intentelo nuevamente");
                             }
@@ -133,7 +140,7 @@ public class Exp2_S4_Antonio_Munoz {
                         }else{
                             System.out.println("Opción no valida, intentelo nuevamente");
                         }  
-                    }
+                    
                     break;
                 case 2:
                     System.out.println("Muchas gracias por su visita");
